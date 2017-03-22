c6e5eaf5b163a15e4890eb7bee0c92887252acaa
Karsten Spakowski <kspakowski@web.de>
2017-03-22 15:32:13 +0100

    addd drop down for site selection

--- src/main/java/com/ge/hc/emrad/xer/controller/ReportingPhysicianController.java
+++ src/main/java/com/ge/hc/emrad/xer/controller/ReportingPhysicianController.java
@@ -1,16 +1,22 @@
 package com.ge.hc.emrad.xer.controller;
 
 import com.ge.hc.emrad.xer.domain.ReportingPhysician;
+import com.ge.hc.emrad.xer.domain.Site;
 import com.ge.hc.emrad.xer.service.ReportingPhysicianService;
+import com.ge.hc.emrad.xer.service.SiteService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by karstenspakowski on 21/03/17.
@@ -28,6 +34,12 @@ public class ReportingPhysicianController {
         this.reportingPhysicianService = reportingPhysicianService;
     }
 
+    private SiteService siteService;
+
+    @Autowired
+    public void setSiteService(SiteService siteService) {
+        this.siteService = siteService;
+    }
 
     @RequestMapping(value = { "", "/" })
     public String index(Model model) {
@@ -47,8 +59,24 @@ public class ReportingPhysicianController {
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String addReportingPhysician(ReportingPhysician reportingPhysician, Model model) {
+        log.debug("add and get");;
+        model.addAttribute("sites", this.siteService.getAllSites());
         model.addAttribute("activePage", "reports");
         return "reports/add";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addSite(@Valid ReportingPhysician reportingPhysician, BindingResult bindingResult, Model model) {
+        log.debug("add (post) new reporting physician");
+        if (bindingResult.hasErrors()) {
+            log.debug("some errors ");
+            model.addAttribute("activePage", "reports");
+            return "reports/add";
+        }
+        reportingPhysician.setLastSynchronized(new Date());
+        reportingPhysician.setCreated(new Date());
+        this.reportingPhysicianService.saveReportingPhysician(reportingPhysician);
+        return "redirect:/reports";
     }
 
     @RequestMapping(value = "/edit/{id}")
